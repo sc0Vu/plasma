863de0dcd0d549b43abfac72546d37ca504fdb06
sc0Vu <alk03073135@gmail.com>
2018-10-22 17:50:14 +0800

    Add solc configuration

--- truffle.js
+++ truffle.js
@@ -1,10 +1,17 @@
 module.exports = {
-    networks: {
-      development: {
-        host: "localhost",
-        port: 8545,
-        gas: 6721975,
-        network_id: "*", // Match any network id
-      }
+  // Solidity compiler configuration
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
+  },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      gas: 6721975,
+      network_id: "*", // Match any network id
+    }
+  }
 };
